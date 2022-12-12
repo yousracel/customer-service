6beecbf114b736d5a2b048e3f63484e16e4f849f
yousra <yousrachafikidrissi@gmail.com>
2022-12-13 00:25:45 +0100

    fix

--- src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -15,7 +17,9 @@ public class CustomerServiceApplication {
 	}
 
 	@Bean
-	CommandLineRunner start(CustomerRepository customerRepository) {
+	CommandLineRunner start(CustomerRepository customerRepository,
+							RepositoryRestConfiguration restConfiguration) {
+		restConfiguration.exposeIdsFor(Customer.class);
 		return args -> {
 			customerRepository.save(new Customer(null,"yousra","yousra@gmail.com"));
 			customerRepository.save(new Customer(null,"mohamed","med@gmail.com"));
